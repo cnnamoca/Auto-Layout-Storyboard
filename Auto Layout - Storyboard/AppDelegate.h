488d943d135d9c3e59ed87a3806f84a1b650ca0e
Carlo <carlo.namoca@gmail.com>
2017-10-10 11:09:16 -0700

    Initial Commit

---
+++ Auto Layout - Storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Auto Layout - Storyboard
+//
+//  Created by Carlo Namoca on 2017-10-10.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Auto Layout - Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Auto Layout - Storyboard
+//
+//  Created by Carlo Namoca on 2017-10-10.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
